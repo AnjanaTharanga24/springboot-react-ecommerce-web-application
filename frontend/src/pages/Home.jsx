871e3118bbb5a685de6ee64ed24f840e2b534661
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-18 18:03:31 +0000

    add 5 cards on the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../css/home.css";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
+    <div className="App">
+      <Navbar />
+
+      <div className=" d-flex">
+        <div className="card shadow card-one col-lg-4 ">
+          
+        </div>
+
+        <div className="">
+          <div className="d-flex">
+            <div className="card shadow card-two col-lg-4 ">
+              
+            </div>
+            <div className="card shadow card-two col-lg-4 ">
+              
+            </div>
+          </div>
+          <div className="d-flex card-sec-two">
+            <div className="card shadow card-two col-lg-4 ">
+              
+            </div>
+            <div className="card shadow card-two col-lg-4 ">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
