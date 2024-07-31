a7b734307ce3024e254c0f3b7d25261af64531d7
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-31 07:39:54 +0000

    add cross origin annotation on top of the user controller

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -5,11 +5,13 @@ import com.example.backend.model.User;
 import com.example.backend.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class UserController{
 
     private UserService userService;
