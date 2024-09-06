a8e9f07ffcb307c3a2c6863e67a3b3798fce8a3b
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-09-06 19:44:47 +0530

    create logic for items find by category

--- backend/src/main/java/com/example/backend/controller/SellerController.java
+++ backend/src/main/java/com/example/backend/controller/SellerController.java
@@ -3,9 +3,12 @@ package com.example.backend.controller;
 import com.example.backend.controller.request.SellerAddNewItemRequest;
 import com.example.backend.controller.response.SellerAddNewItemResponse;
 import com.example.backend.exception.NotFoundException;
+import com.example.backend.model.Item;
 import com.example.backend.service.SellerService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -16,6 +19,11 @@ public class SellerController {
     @PostMapping("/sellers/{user-id}/items")
     public SellerAddNewItemResponse addNewItem(@PathVariable("user-id") Long userId, @RequestBody SellerAddNewItemRequest sellerAddNewItemRequest) throws NotFoundException {
         return sellerService.addNewItem(userId,sellerAddNewItemRequest);
+    }
+
+    @GetMapping("/sellers/items")
+    public List<Item> findByCategroy(@RequestParam("category") String category){
+        return sellerService.itemsFindByCategory(category);
     }
 
 }
--- backend/src/main/java/com/example/backend/repository/ItemRepository.java
+++ backend/src/main/java/com/example/backend/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package com.example.backend.repository;
 import com.example.backend.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item,Long> {
+
+    List<Item> findItemByItemCategory(String category);
 }
