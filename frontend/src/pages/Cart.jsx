dbc38551c197a0469802b20cea27b93c003c51ef
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-09-12 12:31:05 +0530

    add footer to the cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Cart() {
   return (
     <div>
       <Navbar/>
+
+      <div>
+         
+      </div>
+
+      <Footer/>
     </div>
   )
 }
